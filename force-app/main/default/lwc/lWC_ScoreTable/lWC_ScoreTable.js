8df163d14fd293051dab759b27a10931c82faf8b
Trần Đình Phúc <phuctran12421@gmail.com>
2024-01-30 23:05:11 +0700

    LWC --score table -- done

--- force-app/main/default/lwc/lWC_ScoreTable/lWC_ScoreTable.js
+++ force-app/main/default/lwc/lWC_ScoreTable/lWC_ScoreTable.js
@@ -6,48 +6,12 @@ export default class LWC_ScoreTable extends LightningElement {
     @api studentId;
     @api gpa;
     @api totalCredit;
-    // @track subjectScores = [];
-    // @track subjectScoresToView = [];
     @track semesterOptions = [];
     @track data = [];
 
     connectedCallback() {
         this.getSubjectScores();
     }
-
-    // getSubjectScores() {
-    //     getSubjectScore({studentId: this.studentId})
-    //     .then(result => {  
-    //         const semesters = new Map(); 
-    //         this.semesterOptions = [{ label: '全て', value: '' }];
-    //         result.forEach(e => {
-    //             if (!semesters.has(e.Semester_look__r.Id)) {
-    //                 semesters.set(e.Semester_look__r.Id, e.Semester_look__r.Name)
-    //                 this.semesterOptions.push({ label: e.Semester_look__r.Name, value: e.Semester_look__r.Id })
-    //             }
-    //             e.Score__r.forEach(score => {
-    //                 if (score.ExamType__c === 'Progress') {
-    //                     e.progress = score.Score__c
-    //                 } else if (score.ExamType__c === 'Practical') {
-    //                     e.practical = score.Score__c
-    //                 } else if (score.ExamType__c === 'Midterm Exam') {
-    //                     e.midterm = score.Score__c
-    //                 } else if (score.ExamType__c === 'FinalTerm Exam') {
-    //                     e.finnal = score.Score__c
-    //                 }
-    //             })
-    //         })
-    //         this.semesterOptions.sort((a, b) => {
-    //             return a.label.localeCompare(b.label);
-    //         });
-            
-    //         this.subjectScores = result;
-    //         this.subjectScoresToView = this.subjectScores;
-    //     })
-    //     .catch(err => {
-    //         console.log(err.message);
-    //     })
-    // }
 
     getSubjectScores() {
         getSubjectScore({studentId: this.studentId})
@@ -77,7 +41,16 @@ export default class LWC_ScoreTable extends LightningElement {
 
             for (const key of semesters.keys()) {
                 const subjectScores = result.filter(e => e.Semester_look__c === key);
-                this.data.push({show: true, subjectScores, semester: semesters.get(key), id: key})
+                var credit = 0;
+                var avg = 0;
+                subjectScores.forEach(e => {
+                    credit += e.Subject_look__r.CourseCredit__c;
+                    avg += e.AverageScore__c * e.Subject_look__r.CourseCredit__c;
+                })
+                if (credit != 0) {
+                    avg = (avg/credit).toFixed(2);
+                }
+                this.data.push({show: true, subjectScores, semester: semesters.get(key), id: key, avg, credit})
             }
             this.data.sort((a, b) => {
                 return a.semester.localeCompare(b.semester);
